df199f3c2898116d816a2f4931af1e4d4fd5cf82
S0dxu <159276909+S0dxu@users.noreply.github.com>
2025-02-04 10:25:09 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 
 app.use(cors({
-  origin: ["http://localhost:5173", "http://10.5.0.2:5173"],
+  origin: ["http://localhost:5173", "http://10.5.0.2:5173", "https://pixo-v1.netlify.app"],
   methods: ["GET", "POST"],
   allowedHeaders: ["Content-Type"]
 }));
